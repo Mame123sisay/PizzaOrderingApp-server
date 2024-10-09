29c102d9d2ab5837767e1422752b384e893bb977
Mohammed Sisay <155180503+Mame123sisay@users.noreply.github.com>
2024-10-09 15:30:46 +0300

    update server

--- server/pizzas.js
+++
@@ -1,8 +0,0 @@
-// pages/api/pizzas.js
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-      const { name, restaurantId } = req.body;
-      await db.query('INSERT INTO pizzas (name, restaurant_id) VALUES ($1, $2)', [name, restaurantId]);
-      res.status(201).json({ message: 'Pizza added' });
-  }
-}
--- server/toppingss.js
+++
@@ -1,8 +0,0 @@
-// pages/api/toppings.js
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-      const { name, restaurantId } = req.body;
-      await db.query('INSERT INTO toppings (name, restaurant_id) VALUES ($1, $2)', [name, restaurantId]);
-      res.status(201).json({ message: 'Topping added' });
-  }
-}
